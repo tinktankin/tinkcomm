53b3f9af2713b59dd18f1563a529cfc4cac688b4
Megha <megha.bisani@gmail.com>
2020-08-31 23:30:56 +1000

    Added get, update and delete API for subscriber.

--- backend/src/app/api/subscriber.py
+++ backend/src/app/api/subscriber.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework import status
 
 from app.models.company import CompanyModel
 from app.models.subscriber import SubscriberModel
@@ -6,8 +7,8 @@ from app.serializers.subscriber import SubscriberSerializer
 from app.utils.responseutils import send_error_response, send_success_response
 
 
-# List all subscribers, or create a new subscribers.
-# Url: http://<your-domain>/api/v1/subscriber
+# List all subscribers, or create a new subscriber
+# Url: http://<your-domain>/api/v1/subscriber/
 @api_view(['GET','POST'])
 @permission_classes([])
 @authentication_classes([])
@@ -15,7 +16,7 @@ def subscriber_list(request):
     if request.method == 'GET':
         subscribers = SubscriberModel.objects.filter(company_id=request.company_id)
         serializer = SubscriberSerializer(subscribers, many=True)
-        return send_success_response(msg="Subscriber Fetched Successfully", payload=serializer.data)
+        return send_success_response(msg="Subscribers Found", payload=serializer.data)
 
     elif request.method == 'POST':
         company = CompanyModel.objects.get(id=request.company_id, status='ACTIVE')
@@ -23,5 +24,37 @@ def subscriber_list(request):
         serializer = SubscriberSerializer(subscriber, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return send_success_response(msg="Subscriber Added successfully", payload=serializer.data)
-        return send_error_response(msg="Subscriber Not Added", payload=serializer.errors)
+            return send_success_response(msg="Subscriber Created Successfully", payload=serializer.data)
+        return send_error_response(msg="Unable To Add Subscriber", payload=serializer.errors)
+
+
+# Get, Update or Delete Subscriber By Id
+# Url: http://<your-domain>/api/v1/subscriber/<pk>
+# Method: GET, PUT, DELETE
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([])
+@authentication_classes([])
+def subscriber_detail(request, pk):
+    try:
+        subscriber = SubscriberModel.objects.get(pk=pk)
+    except SubscriberModel.DoesNotExist:
+        return send_error_response(msg="Subscriber Not Found", code=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SubscriberSerializer(subscriber)
+        return send_success_response(msg="Subscriber Found", payload=serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SubscriberSerializer(subscriber, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return send_success_response(msg="Subscriber Details Updated Successfully", payload=serializer.data)
+        return send_error_response(msg="Unable To Update Subscriber Details", payload=serializer.errors)
+
+    elif request.method == 'DELETE':
+        operation = subscriber.delete()
+        if operation:
+            return send_success_response(msg="Subscriber Deleted Successfully")
+        else:
+            return send_error_response(msg="Unable To Delete Subscriber")
+
--- backend/src/app/serializers/subscriber.py
+++ backend/src/app/serializers/subscriber.py
@@ -5,5 +5,5 @@ class SubscriberSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubscriberModel
-        fields = ['email','first_name','last_name','full_name', 'status', 'group']
+        fields = ['id','email','first_name','last_name','full_name', 'status', 'group']
         read_only_fields = ['id', 'dateCreated', 'dateModified']
