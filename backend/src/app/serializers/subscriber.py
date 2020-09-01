0331a2b1bbdb2b84419643dcb507c7e19abe9f4e
Megha <megha.bisani@gmail.com>
2020-09-01 16:50:14 +1000

    Added all fields for subscriber

--- backend/src/app/serializers/subscriber.py
+++ backend/src/app/serializers/subscriber.py
@@ -5,5 +5,10 @@ class SubscriberSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SubscriberModel
-        fields = ['id','email','first_name','last_name','full_name', 'status', 'group']
+        fields = ['id','full_name','first_name','middle_name','last_name','email', 'alternate_email',
+                  'phone_number', 'alternate_phone_number','status', 'group', 'company_name', 'designation',
+                  'city', 'address', 'state', 'country', 'zip_code', 'gender', 'title', 'department','university',
+                  'degree', 'passing_year', 'college', 'industry', 'key_skills', 'total_exp', 'years_business',
+                  'turnover', 'date_of_incorporation', 'employees'
+                 ]
         read_only_fields = ['id', 'dateCreated', 'dateModified']
