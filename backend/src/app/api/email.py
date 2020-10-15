08cdcc3064b0431faa6eb1a2f84cbb281ed157db
Megha <megha.bisani@gmail.com>
2020-10-15 16:12:50 +1100

    Refactored Code

--- backend/src/app/api/email.py
+++ backend/src/app/api/email.py
@@ -18,13 +18,16 @@ from app.serializers.email import EmailSerializer
 @permission_classes([])
 @authentication_classes([])
 def config_email(request):
-    company = CompanyModel.objects.get(id=request.company_id, status='ACTIVE')
-    config = EmailConfigModel(company=company)
-    serializer = EmailConfigSerializer(config, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return send_success_response(msg="Email Configuration Created Successfully", payload=serializer.data)
-    return send_error_response(msg="Validation Error", payload=serializer.errors)
+    return create(request)
+
+# Send Email
+# Url: http://<your-domain>/api/v1/email/
+# Method: POST
+@api_view(['POST'])
+@permission_classes([])
+@authentication_classes([])
+def send_email(request):
+    return send_mail_task(request)
 
 class EmailThread(threading.Thread):
     def __init__(self, messages, config, email):
@@ -48,20 +51,23 @@ class EmailThread(threading.Thread):
                 self.email.status = "ERROR"
         self.email.save()
 
-# Send Email
-# Url: http://<your-domain>/api/v1/email/
-# Method: POST
-@api_view(['POST'])
-@permission_classes([])
-@authentication_classes([])
-def send_email(request):
+#Create Email Configuration
+def create(request):
     company = CompanyModel.objects.get(id=request.company_id, status='ACTIVE')
-    config = EmailConfigModel.objects.get(company=company, status='ACTIVE')
+    config = EmailConfigModel(company=company)
+    serializer = EmailConfigSerializer(config, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return send_success_response(msg="Email Configuration Created Successfully", payload=serializer.data)
+    return send_error_response(msg="Validation Error", payload=serializer.errors)
+
+# Send Mail
+def send_mail_task(request):
+    company = CompanyModel.objects.get(id=request.company_id, status='ACTIVE')
     subject = request.data['subject']
     message = request.data['message']
     groups = request.data['groups']
     subscribers = request.data['subscribers']
-    mail_from = config.email_sender
     subscriber_emails = SubscriberModel.objects.none()
 
     if subject and message and (groups or subscribers):
@@ -77,18 +83,25 @@ def send_email(request):
             subscriber_emails = subscriber_emails | receiver
 
         subscriber_emails = subscriber_emails.distinct()
+        return save_and_send_mail(company, groups, subscribers, subject, message, subscriber_emails)
 
-        email = EmailModel(company=company)
-        group_str = ', '.join(map(str, groups))
-        subscribers_str = ', '.join(map(str, subscribers))
-        serializer = EmailSerializer(email, data={'subject': subject, 'message': message, 'groups': group_str,
-                                                  'subscribers': subscribers_str, 'status': "SCHEDULED"})
-        messages = [(subject, message, mail_from, [recipient]) for recipient in subscriber_emails]
-
-        if serializer.is_valid():
-            serializer.save()
-            EmailThread(messages, config, email).start()
-            return send_success_response(msg="Emails send and save successfully")
-        return send_error_response(msg="Validation Error", payload=serializer.errors)
     else:
         return send_error_response(msg="Make sure all fields are entered and valid.")
+
+def save_and_send_mail(company, groups, subscribers, subject, message, subscriber_emails):
+    config = EmailConfigModel.objects.get(company=company, status='ACTIVE')
+    mail_from = config.email_sender
+    email = EmailModel(company=company)
+    group_str = ', '.join(map(str, groups))
+    subscribers_str = ', '.join(map(str, subscribers))
+
+    serializer = EmailSerializer(email, data={'subject': subject, 'message': message, 'groups': group_str,
+                                              'subscribers': subscribers_str, 'status': "SCHEDULED"})
+    messages = [(subject, message, mail_from, [recipient]) for recipient in subscriber_emails]
+
+    if serializer.is_valid():
+        serializer.save()
+        EmailThread(messages, config, email).start()
+        return send_success_response(msg="Emails send and save successfully")
+
+    return send_error_response(msg="Validation Error", payload=serializer.errors)
